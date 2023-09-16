a66108c76b8a6f97a07d06d972eb63387bf3c971
Yevhen Korolov <ekorolev08@gmail.com>
2023-09-16 14:27:48 +0300

    hw-js-09-00

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  btnStart.addEventListener('click', () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  
+    timerId = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+    }, 1000);
+  });
+  
+  btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+  });
