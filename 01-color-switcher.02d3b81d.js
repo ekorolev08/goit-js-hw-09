4cef453c1adbf6cbc4421ee772c9564edd3bd9a7
ekorolev08 <132438091+ekorolev08@users.noreply.github.com>
2023-09-17 13:01:49 +0000

    Deploying to gh-pages from @ ekorolev08/goit-js-hw-09@252355fbf4cb3fd7aec6212658124614c1f39abc 🚀

--- 01-color-switcher.02d3b81d.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,timerId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.02d3b81d.js.map
